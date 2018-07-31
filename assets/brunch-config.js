de095afa9e5d5a2cdcf96081519da55e5afa704e
pedmcor <pedmcor@gmail.com>
2018-07-30 21:34:42 -0300

    Add bulma

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -3,25 +3,10 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
-    },
+   },
     stylesheets: {
       joinTo: "css/app.css"
-    },
+   },
     templates: {
       joinTo: "js/app.js"
     }
@@ -47,6 +32,12 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/bulma/sass"], // Tell sass-brunch where to look for files to @import
+        precision: 8 // Minimum precision required by bootstrap-sass
+      }
     }
   },
 
