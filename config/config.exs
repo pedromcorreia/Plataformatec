8b1aab72ee76b5fd20ee91babfe4f11a1fc20cf1
pedmcor <pedmcor@gmail.com>
2018-08-02 20:20:48 -0300

    Format code

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :to_do_list,
-  ecto_repos: [ToDoList.Repo]
+config :to_do_list, ecto_repos: [ToDoList.Repo]
 
 # Configures the endpoint
 config :to_do_list, ToDoListWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "pml/ez2ojYcGhLtRMQCawTHx0q4v3sTMuEIz7K87SvFvYsuPulsWmKBIkBOxw8dF",
   render_errors: [view: ToDoListWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: ToDoList.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: ToDoList.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,7 +22,7 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
 
 # %% Coherence Configuration %%   Don't remove this line
 config :coherence,
@@ -37,9 +35,18 @@ config :coherence,
   logged_out_url: "/",
   email_from_name: "Your Name",
   email_from_email: "yourname@example.com",
-  opts: [:authenticatable, :recoverable, :lockable, :trackable, :unlockable_with_token, :invitable, :registerable]
+  opts: [
+    :authenticatable,
+    :recoverable,
+    :lockable,
+    :trackable,
+    :unlockable_with_token,
+    :invitable,
+    :registerable
+  ]
 
 config :coherence, ToDoListWeb.Coherence.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
   api_key: "your api key here"
+
 # %% End Coherence Configuration %%
